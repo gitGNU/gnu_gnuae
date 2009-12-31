52a1199be469a19653b45ec0e0d3962596fd942a
Rob Savoye <rob@senecass.com>
2009-12-30 17:59:19 -0700

    use the new loadPage function, rather than cutting and pasting the same code everywhere.

--- php/website/gnuae.js
+++ php/website/gnuae.js
@@ -165,29 +165,7 @@ function updateNEC(op)
   // url += 'awg3=' + document.getElementById('awg_third').value;
   // url += 'wire3=' + document.getElementById('wname_third').value;
 
-  // Open the URL above "asynchronously" (that's what the "true" is
-  // for) using the GET method 
-  xmlhttp.open('GET', url, true);
-  // Check that the PHP script has finished sending us the result
-  xmlhttp.onreadystatechange = function() {
-    if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      // Replace the content of the "result" DIV with the result
-      // returned by the PHP script
-      document.getElementById('result').innerHTML = xmlhttp.responseText + ' ';
-      //document.getElementById('status').innerHTML = 'newProject(' + op + ') done...';
-    } else {
-      if(xmlhttp.readyState == 3) {
-        //document.getElementById('status').innerHTML = 'newProject(' + op + ') in progress...'
-        + xmlhttp.readyState + " : " + xmlhttp.status;
-      } else {
-        // If the PHP script fails to send a response, or sends back an
-        //        error, display a simple user-friendly notification 
-        document.getElementById('status').innerHTML = 'Error: newProject(' + op + ') Failed!'
-        + xmlhttp.readyState + " : " + xmlhttp.status;
-      }
-    }
-  };
-  xmlhttp.send(null);  
+  loadPage(url);
 }
 
 // This updates the information for this project from the web page.
@@ -219,95 +197,40 @@ function updateProfile(op, name)
     document.getElementById('status').innerHTML = 'Writing Profile' + url;
   }
   
-  // alert("URL is " + url);
-  // Open the URL above "asynchronously" (that's what the "true" is
-  // for) using the GET method 
-  xmlhttp.open('GET', url, true);
-  // Check that the PHP script has finished sending us the result
-  xmlhttp.onreadystatechange = function() {
-    if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      // Replace the content of the "result" DIV with the result
-      // returned by the PHP script
-      document.getElementById('result').innerHTML = xmlhttp.responseText + ' ';
-      document.getElementById('status').innerHTML = 'updateProfile(' + op + ') done...';
-    } else {
-      if(xmlhttp.readyState == 3) {
-        document.getElementById('status').innerHTML = 'updateProfile(' + op + ') in progress...'
-        + xmlhttp.readyState + " : " + xmlhttp.status;
-      } else {
-        // If the PHP script fails to send a response, or sends back an
-        //        error, display a simple user-friendly notification 
-        document.getElementById('status').innerHTML = 'Error: updateProfile(' + op + ') Failed!'
-        + xmlhttp.readyState + " : " + xmlhttp.status;
-      }
-    }
-  };
-  // alert("Writing data to database!" + url);  // debugging crap
-  xmlhttp.send(null);
+  loadPage(url);
 }
 
 // This updates the information for this project from the web page.
 function newProject(op)
 {
+
+  // Get the default name and ID from the text field.
   var theID = document.getElementById('projid').value;
   var theName = document.getElementById('projname').value;
-
-  //  alert("Operation is " + op);  // debugging crap
   
   //This sets a variable with the URL (and query strings) to our PHP script
   var url = 'project.php?projid=' + theID;
   url += '&projname=' + theName;
-  url += '&op=' + op;
+  url += '&op=' + op;  
   
-  if (op == 'write') {
-    url += '&projinfo=' + document.getElementById('projinfo').value;
-    url += '&latitude=' + document.getElementById('latitude').value;
-    url += '&longitude=' + document.getElementById('longitude').value;
-    url += '&sunhours=' + document.getElementById('sunhours').value;
-    url += '&windhours=' +  document.getElementById('windhours').value;
-    url += '&speed=' + document.getElementById('speed').value;
-    //    url += '&location=' + document.getElementById('location').value;
-    document.getElementById('status').innerHTML = 'updating Project' + url;
-    //alert("Writing data to database!" + url);  // debugging crap
-  }
-  
-  // Open the URL above "asynchronously" (that's what the "true" is
-  // for) using the GET method 
-  xmlhttp.open('GET', url, true);
-  // Check that the PHP script has finished sending us the result
-  xmlhttp.onreadystatechange = function() {
-    if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      // Replace the content of the "result" DIV with the result
-      // returned by the PHP script
-      document.getElementById('result').innerHTML = xmlhttp.responseText + ' ';
-      document.getElementById('status').innerHTML = 'newProject(' + op + ') done...';
-    } else {
-      if(xmlhttp.readyState == 3) {
-        document.getElementById('status').innerHTML = 'newProject(' + op + ') in progress...'
-        + xmlhttp.readyState + " : " + xmlhttp.status;
-      } else {
-        // If the PHP script fails to send a response, or sends back an
-        //        error, display a simple user-friendly notification 
-        document.getElementById('status').innerHTML = 'Error: newProject(' + op + ') Failed!'
-        + xmlhttp.readyState + " : " + xmlhttp.status;
-      }
-    }
-  };
-  xmlhttp.send(null);
+  loadPage(url);
+  //  alert("Operation is " + op);  // debugging crap
+//  document.projform.projid.value = document.getElementById('editprojid').value;
 }
 
 // This updates the information for this project from the web page.
-function updateProject()
+function updateProject(op)
 {
   var theID = document.getElementById('projid').value;
   var theName = document.getElementById('projname').value;
-  var theInfo = document.getElementById('projinfo').value;
+  //  var theInfo = document.getElementById('projinfo').value;
   var theLat = document.getElementById('latitude').value;
   var theLon = document.getElementById('longitude').value;
-  var theSunHours = document.getElementById('projsunhours').value;
-  var theWindHours = document.getElementById('projwindhours').value;
-  var theSpeed = document.getElementById('projspeed').value;
-  var theLocation = document.getElementById('projlocation').value;
+  alert("Operation is " + op);  // debugging crap
+  var theSunHours = document.getElementById('sunhours').value;
+  var theWindHours = document.getElementById('windhours').value;
+  var theSpeed = document.getElementById('speed').value;
+  var theLocation = document.getElementById('location').value;
 
   document.getElementById('status').innerHTML = "Updating project " + theName
     + ": " + theID + ": " + theInfo + ":" + theLat + ":" + theLon;
@@ -324,31 +247,7 @@ function updateProject()
   url += '&location=' + thelocation;
   url += '&op=write';
 
-  alert("URL is " + url);
-
-  //Open the URL above "asynchronously" (that's what the "true" is for) using the GET method
-  xmlhttp.open('GET', url, true);
-  //Check that the PHP script has finished sending us the result
-  xmlhttp.onreadystatechange = function() {
-    if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      // Replace the content of the "result" DIV with the result
-      // returned by the PHP script
-      document.getElementById('result').innerHTML = xmlhttp.responseText + ' ';
-      document.getElementById('status').innerHTML = 'updateProject() done...'
-      + xmlhttp.readyState + " : " + xmlhttp.status;
-    } else {
-      if(xmlhttp.readyState == 3) {
-        document.getElementById('status').innerHTML = 'updateProject() in progress...'
-        + xmlhttp.readyState + " : " + xmlhttp.status;
-      } else {
-        // If the PHP script fails to send a response, or sends back an
-        //        error, display a simple user-friendly notification 
-        document.getElementById('status').innerHTML = 'Error: updateProject() Failed!'
-        + xmlhttp.readyState + " : " + xmlhttp.status;
-      }
-    }
-  };
-  xmlhttp.send(null);
+  loadPage(url);
 }
 
 // This updates the information for this project from the web page.
@@ -364,60 +263,7 @@ function newLoad()
   //This sets a variable with the URL (and query strings) to our PHP script
   var url = 'newload.php?loadid=' + theID;
   url += '&loadname=' + theQuery;
-  //Open the URL above "asynchronously" (that's what the "true" is
-  //for) using the GET method
-  xmlhttp.open('GET', url, true);
-  //Check that the PHP script has finished sending us the result
-  xmlhttp.onreadystatechange = function() {
-    if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      //Replace the content of the "result" DIV with the result
-      //returned by the PHP script
-      document.getElementById('continue').innerHTML = xmlhttp.responseText + ' ';
-    } else {
-      //If the PHP script fails to send a response, or sends back an
-      //        error, display a simple user-friendly notification
-      //        document.getElementById('result').innerHTML = 'Error: function Failed!';
-      }
-  };
-  xmlhttp.send(null);
+
+  loadPage(url);
 }
 
-// SayHello() prints a message in the document's "hello" area
-function SayHello() {
-    // var theQuery2 = document.getElementById('load').value;
-    var current = document.getElementById("result").innerHTML;
-    // Get the selected item from the menu
-    var theQuery = document.getElementById("load").options[document.getElementById("load").selectedIndex].text;
-    var res = current + "<br>" + theQuery;
-    if(theQuery !== "") {
-      document.getElementById("result").innerHTML=res;
-    }
-    // <?php gui_add_item("TV", "TV sucks", 1, 2, 3, 4); ?>;
-    // <?php $foobar .= " it"; ?>;
-    // foo="<?php gui_get_load_data('TV') ?>";
-    // <?php
-    //     $boo = gui_get_load_data('Laptop');
-    //     //$boo = gui_get_load_data($_GET['test']);
-    //     $ii = count($boo);
-    //     while ($ii) {
-    // ?>
-    // rows2[rows2.length]=<?php echo '"'.$boo[$ii].'"'?>;
-    // <?php
-    //     $ii = $ii - 1;
-    //     }
-    // ?>
-    //foo=<?php echo $foobar ?>;
-    document.getElementById("continue").innerHTML=rows1;
-    
-    rows[rows.length]=theQuery;
-    for (var i in rows) {
-      res = res + "<br>" + rows[i];
-    }    
-    document.getElementById("summary").innerHTML=res;
-    // This array hold all the data for an item
-    var loaditem = [theQuery, 0, 1, 2];
-    allloads[allloads.length] = loaditem;
-    // document.getElementById("continue").innerHTML=allloads;
-
-    return;
-}
